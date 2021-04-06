ff4c0e494d3adfb2973f217a08268b1af1269f19
Prabal Mallick <prabalsslw@gmail.com>
2021-04-06 22:03:20 +0600

    handle error response

--- api/alert.php
+++ api/alert.php
@@ -7,14 +7,21 @@
 	foreach ($server_config as $key => $data) {
 		
 		$server_file = $data['URL'];
+		$status_code = checkHttpStatusCode($server_file);
 		$capacity = $data['Capacity'];
 
-		$parsed_data = parseServerLog($server_file);
+		if($status_code == 200) {
+			$parsed_data = parseServerLog($server_file);
 
-		if(($parsed_data['total']) >= ($capacity/2)) {
-	    	$message = urlencode("<b><u>Caution! Monitor Apache Status</u></b>\n\n<b>Server Name: ".$data['Name']."</b>\n<b>Server IP: </b><code>".$data['Host']."</code>\n<b>Server Capacity: </b><code>".$capacity."</code>\n<b>Threshold: </b><code>".($capacity/2)."</code>\n<b>Load: </b><code>".$parsed_data['total']."</code>\n<b>Processed: </b><code>".$parsed_data['process']."</code>\n<b>Idle: </b><code>".$parsed_data['idle']."</code>\n\n<b><u>Detail Monitoring</u></b>\n\n<b>URL: </b><i>".$data['URL']."</i>" );
+			if(($parsed_data['total']) >= ($capacity/2)) {
+		    	$message = urlencode("<b><u>Caution! Monitor Apache Status</u></b>\n\n<b>Server Name: ".$data['Name']."</b>\n<b>Server IP: </b><code>".$data['Host']."</code>\n<b>Server Capacity: </b><code>".$capacity."</code>\n<b>Threshold: </b><code>".($capacity/2)."</code>\n<b>Load: </b><code>".$parsed_data['total']."</code>\n<b>Processed: </b><code>".$parsed_data['process']."</code>\n<b>Idle: </b><code>".$parsed_data['idle']."</code>\n\n<b><u>Detail Monitoring</u></b>\n\n<b>URL: </b><i>".$data['URL']."</i>" );
+		    }
+		}
+		else {
+			$http_response = get_http_response_code($status_code);
+			$message = urlencode("<b><u>Sorry! Something Went Wrong</u></b>\n\n<b>Server Name: ".$data['Name']."</b>\n<b>URL: </b><code>".$data['URL']."</code>\n\n<b><u>Response</u></b>\n<code>".$status_code." : ".$http_response."</code>" );
+		}
 
-			alertRequest($message);
-	    }
+		alertRequest($message);
 	}
 ?>
